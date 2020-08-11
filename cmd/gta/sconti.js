78e69c38f848e5770269e035bc42dfcbf36ebcfa
Riccardo Fedrizzi <44586474+rikk228@users.noreply.github.com>
2020-08-11 15:08:21 +0200

    Update sconti.js

--- cmd/gta/sconti.js
+++ cmd/gta/sconti.js
@@ -6,7 +6,7 @@ exports.run = async (bot, message, args) => {
   const guild_id_path = `./guild/${message.guild.id}`;
 
     fs.readFile(`${guild_id_path}/sconti.txt`, 'utf8', (err, data) => {
-        if(data === undefined) {
+        if(err) {
           return message.channel.send("Chiedi a moderatori di mettere gli sconti: `.set > sconti`")
         }
         message.channel.send(`${data}`);
